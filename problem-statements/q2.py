b0681ab637fb33fb74272df8eb6f989a96f75824
Manoj Vattikulla <manojvattikulla94409@gmail.com>
2026-02-16 20:58:14 +0530

    q2.py

---
+++ problem-statements/q2.py
@@ -0,0 +1,10 @@
+def convert_seconds(total_seconds: int) -> str:
+
+    # Calculating full minutes
+    minutes = total_seconds // 60
+
+    # Calculating remaining seconds
+    seconds = total_seconds % 60
+
+    # Returning formatted string
+    return f"{minutes}m {seconds}s"
