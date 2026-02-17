c41c1f28359a0f23aa009d7eb8bd0bdb336e7d69
Manoj Vattikulla <manojvattikulla94409@gmail.com>
2026-02-17 20:17:56 +0530

    Update L5.py

--- L5.py
+++ L5.py
@@ -1,1 +1,43 @@
 team name-pywarriors
+def calculate(expression: str) -> float:
+
+    # Removing spaces
+    expression = expression.replace(" ", "")
+
+    stack = []
+    number= 0
+    sign = '+'
+    i = 0
+
+    while i < len(expression):
+        ch = expression[i]
+
+        # Building the number until a operator occurs
+        if ch.isdigit():
+            number= 0
+            
+            while i < len(expression) and expression[i].isdigit():
+                number = number * 10 + int(expression[i])
+                i += 1
+            i -= 1
+
+            if sign == '+':
+                stack.append(number)
+            elif sign == '-':
+                stack.append(-number)
+            elif sign == '*':
+                stack.append(stack.pop() * number)
+            elif sign == '/':
+                stack.append(stack.pop() / number)
+
+        elif ch in '+-*/':
+            sign = ch
+
+        i += 1
+
+    result = sum(stack)
+
+    return round(float(result), 2)
+
+#Team name-{CLP}
+calculate('2 + 3')
